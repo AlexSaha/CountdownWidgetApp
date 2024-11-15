0838012448026ed409714b744df38e6d5d1c009c
AlexS <alexsaha7@gmail.com>
2024-11-15 13:54:25 -0500

    Initial Commit

---
+++ CountdownWidget/CountdownWidgetApp.swift
@@ -0,0 +1,17 @@
+//
+//  CountdownWidgetApp.swift
+//  CountdownWidget
+//
+//  Created by Alex Saha on 11/15/24.
+//
+
+import SwiftUI
+
+@main
+struct CountdownWidgetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
